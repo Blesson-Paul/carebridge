ed9a3ec55a50993ea52bde51f57633978e97d4b8
blesson paul <blempaul@gmail.com>
2026-08-19 16:19:19 +0200

    Restore previous tab order and ensure Active is default without filter param

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -5,7 +5,6 @@ class ConditionsController < ApplicationController
   def index
     @tab = params[:tab].presence || "active"
     all_conditions = current_user.conditions
-    @unarchived_conditions = all_conditions.where(archived: false)
     @active_conditions = all_conditions.where(archived: false, cured: false)
     @cured_conditions = all_conditions.where(archived: false, cured: true)
     @archived_conditions = all_conditions.where(archived: true)
@@ -13,11 +12,9 @@ class ConditionsController < ApplicationController
     @conditions = case @tab
                   when "cured"
                     @cured_conditions
-                  when "all"
-                    @unarchived_conditions
                   when "archived"
                     @archived_conditions
-                  else # "active" is the default
+                  else # "active" default
                     @active_conditions
                   end
   end
