9ca53a2362dbd9cc2701496a7917ef47ecdc1e9d
blesson paul <blempaul@gmail.com>
2026-08-19 14:38:43 +0200

    Add Patient Profile card and symptom tags to dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def calculate_age(dob)
+    return "Not set" unless dob
+    now = Date.current
+    age = now.year - dob.year - (now.strftime('%m%d') < dob.strftime('%m%d') ? 1 : 0)
+    "#{age} yrs"
+  end
 end
